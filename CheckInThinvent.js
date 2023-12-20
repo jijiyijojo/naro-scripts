9e4cb91e5b09b8db07136bdcf0e4d56b2b357927
Naro <leejane311210@gmail.com>
2023-12-20 16:32:36 +0800

    加密 CheckInThinvent.js

--- CheckInThinvent.js
+++ CheckInThinvent.js
@@ -11,12 +11,4 @@ const header =  {
     }
 const payload = process.env.THINVENT_PAYLOAD
 console.log(payload)
-axios.post('http://ehr.thinvent.com:16666/Web.HR//Mobile/AppSign/userSignData',payload,{headers:header} )
-.then((res) => {
-  notify.sendNotify('上班打卡结果',res.data)
-  console.log(`statusCode: ${res.statusCode}`)
-  console.log(res)
-})
-.catch((error) => {
-  console.error(error)
-})
+var _0x4731=['post','log','catch','then'];var _0xe20f=function(_0x4731fd,_0xe20fe1){_0x4731fd=_0x4731fd-0x0;var _0xf7a8cc=_0x4731[_0x4731fd];return _0xf7a8cc;};axios[_0xe20f('0x0')]('http://ehr.thinvent.com:16666/Web.HR//Mobile/AppSign/userSignData',payload,{'headers':header})[_0xe20f('0x3')](_0x18e1f6=>{notify['sendNotify']('上班打卡结果',_0x18e1f6['data']);console[_0xe20f('0x1')]('statusCode:\x20'+_0x18e1f6['statusCode']);console[_0xe20f('0x1')](_0x18e1f6);})[_0xe20f('0x2')](_0x7230e7=>{console['error'](_0x7230e7);});
