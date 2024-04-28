2abb0c7064d2abb7215863a58f052817d7fbbf50
Naro <leejane311210@gmail.com>
2024-04-28 18:58:36 +0800

    自动检测节假日和补班

--- CheckInThinvent.js
+++ CheckInThinvent.js
@@ -7,15 +7,67 @@ const axios = require('axios')
 const header =  {
         "Content-type": "multipart/form-data, boundary=----WebKitFormBoundaryaSp5ER0SytDjXKd1",
         'Cookie': process.env.THINVENT_COOKIE
-
     }
 const payload = process.env.THINVENT_PAYLOAD
+
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1; // 月份从0开始，所以需要+1
+const day = currentDate.getDate();
+const date = `${year}-${month}-${day}`;
+
 function version(){
         return new Promise(function(resolve,reject){
         resolve(axios.get("https://gitee.com/naro_li/statement/raw/main/naro-scripts"))})
 }
 
-async function main(){
-        var _0x4731=['post','log','catch','then'];var _0xe20f=function(_0x4731fd,_0xe20fe1){_0x4731fd=_0x4731fd-0x0;var _0xf7a8cc=_0x4731[_0x4731fd];return _0xf7a8cc;};axios[_0xe20f('0x0')]('http://ehr.thinvent.com:16666/Web.HR//Mobile/AppSign/userSignData',payload,{'headers':header})[_0xe20f('0x3')](_0x18e1f6=>{notify['sendNotify']('上班打卡结果',_0x18e1f6['data']);console[_0xe20f('0x1')]('statusCode:\x20'+_0x18e1f6['statusCode']);console[_0xe20f('0x1')](_0x18e1f6);})[_0xe20f('0x2')](_0x7230e7=>{console['error'](_0x7230e7);});
+function checkHoliday(){
+        return new Promise(function(resolve,reject){
+        resolve(
+        axios.get(`http://timor.tech/api/holiday/info/2024-04-29`, {
+          headers: {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Accept-Encoding': 'gzip, deflate',
+            'If-Modified-Since': 'Sat, 27 Apr 2024 22:01:35 GMT',
+            'Host':'timor.tech',
+            'Proxy-Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux mips64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.125 Safari/537.36'
+          }
+        }))})
 }
+
+async function main(){
+    await version().then(data=>{console.log(data.data)})
+    await checkHoliday().then(res=>{       
+            if (res.data.code==0){
+                holiday_type = res.data.type
+                if (holiday_type.type == 1 ){
+                    console.log('今天是周末！好好享受生活吧！')
+                    
+                    }
+                if (holiday_type.type == 2){
+                    console.log('今天是'+holiday_type.name+'!假期快乐！')
+                    notify.sendNotify('打卡情况','今天是'+holiday_type.name+'!假期快乐！\n如果你不幸要加班，那么你的加班工资是'+res.data.holiday.wage+'倍')
+                    
+                }
+                if (holiday_type.type == 0 || holiday_type.type === 3){
+                    console.log('今天是'+holiday_type.name+'!快去打卡上班吧!')
+                      axios.post('http://ehr.thinvent.com:16666/Web.HR//Mobile/AppSign/userSignData',payload,{headers:header} )
+                        .then((res) => {
+                        notify.sendNotify('上班打卡结果',res.data)
+                        console.log(`statusCode: ${res.statusCode}`)
+                        console.log(res)
+                        })
+                        .catch((error) => {
+                        console.error(error)
+                        })
+                }
+
+            }
+    })
+ }
+
 main()
